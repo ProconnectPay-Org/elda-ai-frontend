d76208b01ee57b7d6d72be74b630a9d8f59fbde1
Ivylegend <victoryicha2@gmail.com>
2024-07-16 08:42:53 +0100

    change logos

--- src/components/HomeNav.tsx
+++ src/components/HomeNav.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import Logo from "../assets/elda.png";
+import Logo from "../assets/elda-logo.svg";
 import { Button } from "./ui/button";
 
 const HomeNavBar = () => {
