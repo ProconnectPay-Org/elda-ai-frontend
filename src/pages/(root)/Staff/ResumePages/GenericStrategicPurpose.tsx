c7e125a9fc1e83c0dc07e5b38a82775f6bf890d3
Ivylegend <victoryicha2@gmail.com>
2024-07-31 16:16:07 +0100

    add refine resume pages

--- src/pages/(root)/Staff/ResumePages/GenericStrategicPurpose.tsx
+++ src/pages/(root)/Staff/ResumePages/GenericStrategicPurpose.tsx
@@ -1,8 +1,52 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { getErrorMessage } from "@/lib/utils";
+import { ResumeStep5FormData } from "@/types";
+import { useFormContext } from "react-hook-form";
+import "react-phone-input-2/lib/style.css";
 
 const GenericStrategicPurpose = () => {
-  return (
-    <div>GenericStrategicPurpose</div>
-  )
-}
+  const {
+    register,
+    // control,
+    formState: { errors },
+  } = useFormContext<ResumeStep5FormData>();
 
-export default GenericStrategicPurpose
+  return (
+    <div className="space-y-5">
+      <div>
+        <h2 className="font-bold">CAREER STRATEGIC PURPOSE AI PROMPT</h2>
+        <div className="space-y-2">
+          <label htmlFor="">Type of program</label>
+          <Input type="email" className="rounded-full" />
+          <small>Choose the correct the program</small>
+        </div>
+      </div>
+      <div className="border border-pale-bg bg-gray py-9 px-5 sm:px-10 rounded-2xl md:rounded-3xl space-y-5">
+        <p className="text-center text-red font-bold text-2xl">
+          CAREER STRATEGIC PURPOSE
+        </p>
+        <div className="flex flex-col">
+          <label htmlFor="prompt">Prompt</label>
+          <Textarea
+            className="border border-gray-border rounded-2xl py-2 px-4"
+            id="prompt"
+            {...register("prompt")}
+            placeholder="Enter your prompt"
+          />
+          {errors.prompt && (
+            <span className="text-red text-sm">
+              {getErrorMessage(errors.prompt)}
+            </span>
+          )}
+        </div>
+        <div className="flex justify-end">
+          <Button className="bg-red">Generate</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GenericStrategicPurpose;
