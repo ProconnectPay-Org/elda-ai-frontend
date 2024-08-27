a358de68436f315f89028c894ff98082b4e4c54e
Ivylegend <victoryicha2@gmail.com>
2024-08-27 16:05:17 +0100

    candidate page

--- src/pages/(root)/Candidates/CandidateStatus.tsx
+++ src/pages/(root)/Candidates/CandidateStatus.tsx
@@ -1,9 +1,22 @@
-import CandidateLayout from "@/layouts/CandidateLayout"
+import CandidateLayout from "@/layouts/CandidateLayout";
 
 const CandidateStatus = () => {
   return (
-    <CandidateLayout>CandidateStatus</CandidateLayout>
-  )
-}
+    <CandidateLayout>
+      <section className="md:w-[880px] mx-auto space-y-10">
+        <h1 className="font-bold text-4xl">Welcome, Grace</h1>
 
-export default CandidateStatus
+        <div className="h-[80px] rounded-2xl w-full p-5 bg-gradient-to-r from-red to-[#919293] flex items-center">
+          <p className="text-white font-medium text-2xl">Not Completed</p>
+        </div>
+
+        <div>
+
+        </div>
+
+      </section>
+    </CandidateLayout>
+  );
+};
+
+export default CandidateStatus;
