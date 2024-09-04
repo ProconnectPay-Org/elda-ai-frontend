0d8c6d79b645e2639e3bd341debc0996c2111fba
Ivylegend <victoryicha2@gmail.com>
2024-09-04 15:13:45 +0100

    adjust date select field width

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -6,10 +6,10 @@ import { AuthLayoutProps } from "@/types";
 const AuthLayout = ({ children }: AuthLayoutProps) => {
   return (
     <div className="sign-in-layout">
-      <nav className="flex justify-between items-center w-full px-8 py-2 h-[14vh] md:h-[120px] bg-white">
+      <nav className="flex justify-between items-center w-full px-8 py-2 h-[15vh] md:h-[120px] bg-white">
         <Link
           to="/"
-          className="cursor-pointer flex items-center h-[120px] md:h-[100px] md:w-[200px] gap-1"
+          className="w-[40%] sm:w-[30%] md:w-[15%]"
         >
           <img src={Logo} alt="logo" className="w-full h-full object-cover" />
         </Link>
