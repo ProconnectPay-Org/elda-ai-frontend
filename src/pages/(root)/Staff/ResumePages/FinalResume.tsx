4467034491ac9c3c998b020cdf564d6d483b6ce5
Ivylegend <victoryicha2@gmail.com>
2025-02-20 18:08:13 +0100

    resume for resume flow

--- src/pages/(root)/Staff/ResumePages/FinalResume.tsx
+++ src/pages/(root)/Staff/ResumePages/FinalResume.tsx
@@ -2,11 +2,8 @@ import RootLayout from "@/layouts/RootLayout";
 import { Button } from "@/components/ui/button";
 import { Link, useParams } from "react-router-dom";
 import { ArrowLeft } from "lucide-react";
-import NewResumePdf from "@/components/NewReumePdf";
 import { useRef } from "react";
-import html2canvas from "html2canvas";
-import jsPDF from "jspdf";
-import { useCandidates } from "@/hooks/useCandidiates";
+import ResumePdf from "@/components/ResumePdf";
 
 const FinalResume = () => {
   const { id } = useParams<{ id: string }>();
@@ -17,78 +14,8 @@ const FinalResume = () => {
     return;
   }
 
-  const { singleCandidate } = useCandidates(id);
-
-  const downloadPDF = async () => {
-    localStorage.setItem("resumeCurrentPage", "0");
-    if (resumeRef.current) {
-      const canvas = await html2canvas(resumeRef.current, {
-        scale: 2,
-        backgroundColor: "#ffffff",
-      });
-
-      const pdf = new jsPDF("p", "mm", "a4");
-      const pdfWidth = pdf.internal.pageSize.getWidth();
-      const pdfHeight = pdf.internal.pageSize.getHeight();
-
-      const topMargin = 0;
-      const bottomMargin = 8;
-
-      const canvasWidth = canvas.width;
-      const canvasHeight = canvas.height;
-      // Calculate the height of content to fit per page (excluding margins)
-      const contentHeightPerPage = pdfHeight - bottomMargin;
-
-      const totalPDFPages = Math.ceil(
-        (canvasHeight * (pdfWidth / canvasWidth)) / contentHeightPerPage
-      );
-
-      for (let page = 0; page < totalPDFPages; page++) {
-        const sourceY = page * contentHeightPerPage * (canvasWidth / pdfWidth);
-        const pageCanvas = document.createElement("canvas");
-        pageCanvas.width = canvasWidth;
-        pageCanvas.height = contentHeightPerPage * (canvasWidth / pdfWidth);
-
-        const context = pageCanvas.getContext("2d");
-        if (context) {
-          context.fillStyle = "#ffffff"; // White background
-          context.fillRect(0, 0, pageCanvas.width, pageCanvas.height);
-
-          context.drawImage(
-            canvas,
-            0,
-            sourceY,
-            canvasWidth,
-            pageCanvas.height,
-            0,
-            0,
-            canvasWidth,
-            pageCanvas.height
-          );
-
-          const pageImgData = pageCanvas.toDataURL("image/jpeg", 1.0);
-          if (page > 0) pdf.addPage(); // Add new page for each additional slice
-
-          // Apply top margin only for subsequent pages
-          const yPosition = page > 0 ? topMargin : 0; // No margin for the first page
-
-          pdf.addImage(
-            pageImgData,
-            "JPEG",
-            0,
-            yPosition,
-            pdfWidth,
-            contentHeightPerPage
-          );
-        }
-      }
-
-      const fileName = singleCandidate?.user?.full_name
-        ? `${singleCandidate.user.full_name} Resume.pdf`
-        : "Resume.pdf";
-
-      pdf.save(fileName);
-    }
+  const downloadResume = () => {
+    window.print();
   };
 
   return (
@@ -110,7 +37,9 @@ const FinalResume = () => {
       </div>
 
       <div ref={resumeRef} className="max-w-3xl m-auto">
-        <NewResumePdf />
+        <div className="resume-print">
+          <ResumePdf />
+        </div>
       </div>
 
       <div className="mt-5 md:mt-8 w-full flex items-center justify-between">
@@ -120,7 +49,7 @@ const FinalResume = () => {
           </Button>
         </Link>
 
-        <Button onClick={downloadPDF} className="bg-red">
+        <Button onClick={downloadResume} className="bg-red">
           Download Resume
         </Button>
       </div>
